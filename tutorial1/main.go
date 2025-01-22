64be49a45c1b2dda11017119fa7ccbc799c1ac85
Juho Choi <juho.c98@gmail.com>
2025-01-22 18:34:56 -0500

    .

--- tutorial1/main.go
+++ tutorial1/main.go
@@ -3,43 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	// var intArr [3]int32 = [3]int32{1,2,3}
-	// intArr2 := [3]int32{1,2,3}
-	// intArr3 := [...]int32{1,2,3}
-		
-	// fmt.Println(intArr)
-	// fmt.Println(intArr2)
-	// fmt.Println(intArr3)
-
-	var intSlice []int32 = []int32{4, 5, 6}
-	fmt.Printf("The length is %v with capacity %v", len(intSlice), cap(intSlice))
-	intSlice = append(intSlice, 7)
-	fmt.Printf("\nThe length is %v with capacity %v\n", len(intSlice), cap(intSlice))
-
-	var intSlice2 []int32 = []int32{8, 9}
-	intSlice = append(intSlice, intSlice2...)
-	fmt.Println(intSlice)
-
-	var intSlice3 []int32 = make([]int32, 3, 8)
-	fmt.Println(intSlice3)
-
-	var myMap map[string]uint8 = make(map[string]uint8)
-	fmt.Println(myMap)
-
-	var myMap2 = map[string]uint8{"Adam":23, "Sarah": 45, "John": 40, "Chloe": 30}
-	fmt.Println(myMap2["Adam"])
-
-	var age, ok = myMap2["Jason"]
-
-	if ok {
-		fmt.Printf("The age is %v\n", age)
-	} else {
-		fmt.Printf("Invalid Name\n")
+	var intSlice = []int{1, 2, 3}
+	fmt.Println(sumIntSlice(intSlice))
+}
+func sumIntSlice(slice []int) int{
+	var sum int
+	for _, v := range slice{
+		sum += v
 	}
+	return sum
+}
 
-	delete(myMap2, "Adam")
-
-	for name, age:= range myMap2{
-		fmt.Printf("Name: %v, Age: %v\n", name, age)
+func sumSlice[T int | float32 | float64](slice []T) T{
+	var sum T
+	for _, v := range slice{
+		sum += v
 	}
+	return sum
 }
